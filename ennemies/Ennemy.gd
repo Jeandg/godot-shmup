7b1706a7179623c614a8e320e693f87bf3fb60b0
Jean di Giovanni <jeandigiovanni@gmail.com>
2024-07-02 21:40:18 +0200

    Ennemy path

--- ennemies/Ennemy.gd
+++ ennemies/Ennemy.gd
@@ -5,10 +5,22 @@ var HIT_DURATION = 0.1
 
 @export var health = 3
 @export var destroy_score = 5000
+@export var time_to_go_through_path = 10
+var time_elapsed = 0
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var path = $Path
+@onready var path_follow = $Path2D/PathFollow2D
 
 @onready var explosion_scene = preload("res://explosion/explosion.tscn")
+
+var offset = Vector2()
+
+func _ready():
+	offset = Vector2(position.x, position.y)
+	# path_follow.h_offset = position.x
+	# path_follow.v_offset = position.y
+	pass
 
 func _process(delta):
 	last_hit += delta
@@ -17,8 +29,14 @@ func _process(delta):
 	
 	if last_hit > HIT_DURATION:
 		animated_sprite.material.set_shader_parameter("damaged", false)
-		
-	position = Vector2(position.x, position.y + delta * 40)
+	
+	time_elapsed += delta
+	
+	var progress = time_elapsed / time_to_go_through_path
+	
+	path_follow.progress_ratio = progress
+	position = path_follow.position + offset 
+	print(path_follow.position)
 
 func _check_health():
 	if health <= 0:
