844ad98e4dedbf3f4395f90c13a6312954e249b0
Jean di Giovanni <jeandigiovanni@gmail.com>
2024-07-11 21:25:58 +0200

    Reconnect life bar

--- levels/level_1/LifeProgressBar.gd
+++ levels/level_1/LifeProgressBar.gd
@@ -1,8 +1,17 @@
 extends TextureProgressBar
 
+@export var health_component: HealthComponent
+
 func _ready():
+	if health_component:
+		health_component.health_depleted.connect(_on_health_depleted)
+		health_component.health_healed.connect(_on_health_healed)
+		max_value = 100
+		value = max_value
 	set_process(true)
-	
-func _process(delta):
-	value = GameState.player_life
-	max_value = 100
+
+func _on_health_depleted(new_health, amount_depleted):
+	value = new_health
+
+func _on_health_healed(new_health, amount_healed):
+	value = new_health
